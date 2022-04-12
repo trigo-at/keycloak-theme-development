bf86c7a870aa39f36014d19e17e3c4547d5df451
David Wippel <david@trigo.at>
2022-04-12 12:38:51 +0200

    fix: fixes wrong param for docker-compose start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,4 +3,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker-compose -f ${DIR}/../docker-compose.yml up -p
+docker-compose -f ${DIR}/../docker-compose.yml up -d
