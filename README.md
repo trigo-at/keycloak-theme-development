9e6e0b60aa02a4470f579a0cf36bee70059ec93e
adrianJ96 <adrianjoos@gmail.com>
2024-05-07 08:54:48 +0200

    README/scripts update

--- README.md
+++ README.md
@@ -8,18 +8,24 @@ An example development environment setup to build Keycloak themes
 
 Start required container (keycloak+postgres) using the follow command:
 
-`./scripts/start.sh`
+`docker compose up -d` 
 
-Access the Administration Console via `http://localhost:8080/auth/`
+You can proof if the container is running with the command:
+
+`docker ps` 
+
+Access the Administration Console via `http://localhost:8080/`
 
 ### (optional) Extract themes
 
 This step is optional, although I strongly suggest extracting the themes from the container.
 They are often the only reliable source on how things are implemented.
 
-`./scripts/exportThemes.sh`
+You can find a couple of helpful scripts in the scripts folder.
 
-Themes are extracted to `./themes/keycloak-provided-themes`.
+The jar can be extracted with this command:
+
+`./scripts/exportThemes.sh`
 
 ## Build the Tailwind Example
 
--- scripts/exportThemes.sh
+++ scripts/exportThemes.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+# Determine the path of the current script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo ${DIR}
 
+# Remove the existing themes directory
 rm -rf ${DIR}/../themes/keycloak-provided-themes
-mkdir -p ${DIR}/../themes/
 
-docker cp $jar $(docker ps | grep jboss/keycloak:15.1.0 | cut -d' ' -f1):/opt/jboss/keycloak/themes ${DIR}/../themes/keycloak-provided-themes
+# Create the directory for the themes
+mkdir -p ${DIR}/../themes/keycloak-provided-themes
+
+# Determine the name of the JAR file
+JAR_FILE="org.keycloak.keycloak-themes-24.0.2.jar"
+
+# Determine the Container ID of the running Keycloak container
+CONTAINER_ID=$(docker ps | grep quay.io/keycloak/keycloak:24.0.2 | awk '{print $1}')
+
+# Copy the JAR file from the container to the target directory
+docker cp ${CONTAINER_ID}:/opt/keycloak/lib/lib/main/${JAR_FILE} ${DIR}/../themes/keycloak-provided-themes
